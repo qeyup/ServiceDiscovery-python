a9130436217996e8b1bc49757048edc4aefbd02d
JMG <jgmore@gmail.com>
2024-01-02 23:57:10 +0000

    upload note

--- updatePackage.sh
+++ updatePackage.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+# Upload package note:
+# Generate token and set file -> $HOME/.pypirc
+#
+# [pypi]
+#   username = __token__
+#   password = pypi-XXXXXXXX
+#
+
 # Generate package
 ./setup.py sdist bdist_wheel
 
